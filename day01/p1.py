02a694fdc8459a7a40df63fcd7e4cc8d8ee09669
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-12-01 22:56:07 +0000

    input is the same; p2 is similar

--- day01/p1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-pairs = [list(map(int, line.split('   '))) for line in open(0).read().splitlines()]
-s = [sorted([p[i] for p in pairs]) for i in [0, 1]]
-# print(sum([abs(p[0] - p[1]) for p in s01]))
-print(s)
-print(sum([abs(p[0] - p[1]) for p in zip(s[0], s[1])]))
